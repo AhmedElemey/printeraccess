2412ad69221207d08063c2d7cf97f5496e635a18
AhmedElelemy <ahmedkajoahmedkajo@gmail.com>
2021-05-11 03:13:41 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/printeraccess/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.printeraccess
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:printeraccess/print.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    // setState(() {
+    //   _counter++;
+    // });
+    List<String> myList = [];
+    for (int i = 0; i < 4; ++i) {
+      myList.add('Item nr $i');
+    }
+    Navigator.push(
+        context,
+        CupertinoPageRoute(
+          builder: (context) => PrintOrders(
+            orderType: 'demo',
+            orderNumber: '123456',
+            customerName: 'John',
+            deliveryTime: 'asap',
+            instruction: 'Are you going to be able to print this one?',
+            items: myList,
+          ),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
